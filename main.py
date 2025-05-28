dc4ea70a0fc9c35d512aafae994633c966b0643f
dryoney <74609294+Dryoney@users.noreply.github.com>
2025-05-28 18:13:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from collections import defaultdict, deque
+
+
 def check_relation(net: tuple, person_name: str, friend_name: str) -> bool:
     graph = defaultdict(set)
     for user1, user2 in net:
         graph[user1].add(user2)
         graph[user2].add(user1)
-
     queue = deque([person_name])
     visited = {person_name}
-
     while queue:
         current_user = queue.popleft()
         if current_user == friend_name:
@@ -17,6 +17,7 @@ def check_relation(net: tuple, person_name: str, friend_name: str) -> bool:
                 visited.add(friend)
                 queue.append(friend)
     return False
+
 
 if __name__ == '__main__':
     net = (
